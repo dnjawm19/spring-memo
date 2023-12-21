b49d8dc77075d87174c529fd85549172d3bc2bb4
jeongsoo <dnjawm19@naver.com>
2023-12-21 16:24:35 +0900

    Create(Update, Delete)

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -35,4 +35,26 @@ public class MemoController {
 
         return responseDtoList;
     }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        if(memoList.containsKey(id)) {
+            Memo memo = memoList.get(id);
+
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("해당 메모가 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public String deleteMemo(@PathVariable Long id) {
+        if(memoList.containsKey(id)) {
+            memoList.remove(id);
+            return id+"번 메모가 삭제되었습니다.";
+        } else {
+            throw new IllegalArgumentException("해당 메모가 존재하지 않습니다.");
+        }
+    }
 }
